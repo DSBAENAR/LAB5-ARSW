702e76b4d060d08d8c4b7934cc936658deac5dac
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-09-28 22:42:23 -0500

    Fix API endpoint URL in apiclient module

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -1,5 +1,5 @@
 var apiclient = (function(){
-    const url = "blueprints-e2f7gcd4c0aqcsg2.canadacentral-01.azurewebsites.net/api/blueprints";
+    const url = "https://blueprints-e2f7gcd4c0aqcsg2.canadacentral-01.azurewebsites.net/api/blueprints";
 
     return {
         getBlueprintsByAuthor: function(author, callback){
